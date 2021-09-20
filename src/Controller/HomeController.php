975e208daa174cc365072d6632feeb6e4bb8bc1a
Joskel Tagueu <joskeltagueu@gmail.com>
2021-09-20 17:50:31 +0100

    starting integrating controller

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace myLaw\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Description of HomeController
+ *
+ * @author tagueu
+ */
+
+
+class HomeController extends Controller{
+    //put your code here
+    
+    public function indexAction(){
+        
+    }
+}
